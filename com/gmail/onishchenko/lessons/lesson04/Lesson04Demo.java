b7cfef9f0fb3edb783192afb721ef2a7c8dd5511
Onishehnko Oleksii <test@gmail.com>
2024-05-23 21:08:35 +0300

    Lesson-04 - Pre and post increments

--- com/gmail/onishchenko/lessons/lesson04/Lesson04Demo.java
+++ com/gmail/onishchenko/lessons/lesson04/Lesson04Demo.java
@@ -3,5 +3,36 @@ package com.gmail.onishchenko.lessons.lesson04;
 public class Lesson04Demo {
     public static void main(String[] args) {
         String greeting = "Hello World! My name is Oleksii";
+
+        int a = 42;
+        int b = -a;
+        System.out.println("a is " + a);
+        System.out.println("-a is " + b);
+
+        b = 10;
+        System.out.println("42 % 10 is " + b); // 42 - (42 / 10) * 10 == 42 - 4 * 10 == 2
+        int result = 10 % 42;
+        System.out.println("10 % 42 is " + result);
+
+//        a = a + 10;
+        a += 10;
+        System.out.println("a = " + a);
+        a = a + 1;
+        a += 1;
+        a = 10;
+        a++;  // post-increment
+        System.out.println("post-increment: " + a);
+        a = 10;
+        ++a;  // pre-increment
+        System.out.println("pre-increment: " + a);
+
+        a = 10;
+        result = ++a;
+        System.out.println("pre-increment: " + result);
+
+        a = 10;
+        result = a++ + 100;
+        System.out.println("post-increment: " + result);
+        System.out.println("post-increment (a): " + a);
     }
 }
