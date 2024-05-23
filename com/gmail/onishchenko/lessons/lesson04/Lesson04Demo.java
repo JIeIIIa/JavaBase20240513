2ccbc8c09c2b9b237dcfb0212c462532caf65487
Onishehnko Oleksii <test@gmail.com>
2024-05-23 20:28:48 +0300

    Lesson-04 - Add another greeting

--- com/gmail/onishchenko/lessons/lesson04/Lesson04Demo.java
+++ com/gmail/onishchenko/lessons/lesson04/Lesson04Demo.java
@@ -1,4 +1,7 @@
 package com.gmail.onishchenko.lessons.lesson04;
 
 public class Lesson04Demo {
+    public static void main(String[] args) {
+        String greeting = "My name is Oleksii";
+    }
 }
