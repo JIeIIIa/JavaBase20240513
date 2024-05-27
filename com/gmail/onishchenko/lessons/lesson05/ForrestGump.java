2e462caccdb4820fe609ee3805ade553a9521a2c
Onishehnko Oleksii <test@gmail.com>
2024-05-27 20:14:16 +0300

    Lesson-05 - if-else-if

--- com/gmail/onishchenko/lessons/lesson05/ForrestGump.java
+++ com/gmail/onishchenko/lessons/lesson05/ForrestGump.java
@@ -2,16 +2,22 @@ package com.gmail.onishchenko.lessons.lesson05;
 
 public class ForrestGump {
     public static void main(String[] args) {
-        int menPower = 42;
         int dangerLevel = 42;
+        int menPower = 50;
+        int highDander = 100;
 
         System.out.println("Men's poser is " + menPower);
         if (menPower > dangerLevel) {
-            System.out.println("!!! DANGER!!!");
+            if (menPower > highDander) {
+                System.out.println("!!! DANGER!!!");
+            }
             System.out.println("Run, Forrest, run!!!");
+        } else if (menPower > 0) {
+            System.out.println("Be ready");
         } else {
             System.out.println("All FINE)");
         }
+
 
         System.out.println("The end");
     }
