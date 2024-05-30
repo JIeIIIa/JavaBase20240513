8c497e02ed11c5959835f61ad1da15f6692549f1
Onishehnko Oleksii <test@gmail.com>
2024-05-30 19:59:25 +0300

    Lecture-06 - For loop

--- com/gmail/onishchenko/lessons/lesson06/MultiplyTable.java
+++ com/gmail/onishchenko/lessons/lesson06/MultiplyTable.java
@@ -8,18 +8,18 @@ public class MultiplyTable {
         System.out.println("Input limit");
         int limit = scanner.nextInt();
 
-        if (limit >= 1) {
-            System.out.println("1\t2\t3\t4\t5\t6\t7\t8\t9\t10");
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(makeLine(i));
         }
-        if (limit >= 2) {
-            System.out.println("2\t4\t6\t8\t10\t12\t14\t16\t18\t20");
+    }
+
+    public static String makeLine(int base) {
+        String result = "";
+        for (int i = 1; i <= 10; i++) {
+            result += base * i + "\t";
         }
-        if (limit >= 3) {
-            System.out.println("3\t6\t9\t12\t15\t18\t21\t24\t27\t30");
-        }
-        if (limit >= 4) {
-            System.out.println("4\t8\t12\t16\t20\t24\t28\t32\t26\t40");
-        }
+
+        return result;
     }
 
 
