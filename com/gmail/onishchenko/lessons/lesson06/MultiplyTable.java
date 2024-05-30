145db0c81aba42961eb531a57d18aa4b164d5fa2
Onishehnko Oleksii <test@gmail.com>
2024-05-30 20:12:28 +0300

    Lecture-06 - For loop (part-2)

--- com/gmail/onishchenko/lessons/lesson06/MultiplyTable.java
+++ com/gmail/onishchenko/lessons/lesson06/MultiplyTable.java
@@ -8,13 +8,19 @@ public class MultiplyTable {
         System.out.println("Input limit");
         int limit = scanner.nextInt();
 
-        for (int i = 1; i <= limit; i++) {
+        int i = 1;
+        for (; ; ) {
+            if (!(i <= limit)) {
+               break;
+            }
             String result = "";
             for (int j = 1; j <= 10; j++) {
                 result += i * j + "\t";
             }
             System.out.println(result);
+            i++;
         }
+        System.out.println("The end");
     }
 
 }
