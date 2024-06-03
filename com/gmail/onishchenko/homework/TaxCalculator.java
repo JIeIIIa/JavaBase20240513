5c99d001877935f5ef183ea40bb69317cbabea04
Onishehnko Oleksii <test@gmail.com>
2024-06-03 19:34:15 +0300

    Homework-5.1 - Tax calculator

---
+++ com/gmail/onishchenko/homework/TaxCalculator.java
@@ -0,0 +1,27 @@
+package com.gmail.onishchenko.homework;
+
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your profit: ");
+        double profit = scanner.nextDouble();
+
+
+        if (profit < 0) {
+            System.out.println("Incorrect data (((");
+        } else {
+            double tax;
+            if (profit < 10_000) {
+                tax = profit * 0.025;
+            } else if (profit < 25_000) {
+                tax = profit * 0.043;
+            } else {
+                tax = profit * 0.067;
+            }
+
+            System.out.printf("Your tax == %.2f", tax);
+        }
+    }
+}
