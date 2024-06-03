222d5af8d2b2a9f03bf882c74ead5a6cfa188f45
Onishehnko Oleksii <test@gmail.com>
2024-06-03 21:22:52 +0300

    Lesson-7 - Random values

--- com/gmail/onishchenko/lesson07/Fibonacci.java
+++ com/gmail/onishchenko/lesson07/Fibonacci.java
@@ -1,11 +1,22 @@
 package com.gmail.onishchenko.lesson07;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        int elementCount = 30;
-        long[] numbers = new long[elementCount];
-        numbers[0] = 1;
-        numbers[1] = 1;
+        int maxSize = 30;
+        int elementCount = ThreadLocalRandom.current().nextInt(2, maxSize + 1);
+        Random random = new Random();
+        elementCount = random.nextInt(2, maxSize + 1);
+        System.out.println("Random value: " + elementCount);
+        elementCount = random.nextInt(2, maxSize + 1);
+        System.out.println("Random value: " + elementCount);
+//        long[] numbers = new long[elementCount];
+//        numbers[0] = 1;
+//        numbers[1] = 1;
+
+        long[] numbers = {1, 1, 0, 0, 0};
         for (int i = 2; i < numbers.length; i++) {
             numbers[i] = numbers[i - 1] + numbers[i - 2];
         }
