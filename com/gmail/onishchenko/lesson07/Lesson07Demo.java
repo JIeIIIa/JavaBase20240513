8f2098de774ef8a1aa2352feb99d71a2cb433c74
Onishehnko Oleksii <test@gmail.com>
2024-06-03 19:56:15 +0300

    Lesson-7 - Array problem

---
+++ com/gmail/onishchenko/lesson07/Lesson07Demo.java
@@ -0,0 +1,13 @@
+package com.gmail.onishchenko.lesson07;
+
+public class Lesson07Demo {
+    public static void main(String[] args) {
+        int firstTourist = 20;
+        int secondTourist = 20;
+        int thirdTourist = 22;
+        int fourthTourist = 20;
+
+        double averageAge = (double) (firstTourist + secondTourist + thirdTourist + fourthTourist) / 4;
+        System.out.println("Average age is: " + averageAge);
+    }
+}
