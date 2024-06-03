a73a0bd4529834ccde966148681da36363db9db4
Onishehnko Oleksii <test@gmail.com>
2024-06-03 20:21:48 +0300

    Lesson-7 - Array (part-3)

--- com/gmail/onishchenko/lesson07/Lesson07Demo.java
+++ com/gmail/onishchenko/lesson07/Lesson07Demo.java
@@ -2,10 +2,17 @@ package com.gmail.onishchenko.lesson07;
 
 public class Lesson07Demo {
     public static void main(String[] args) {
-        int[] ages = {16, 20, 22, 43};
+        int size = 14;
+        int[] ages = {16, 20, 22, 43, 12, 42, 53, 54};
 
-        double averageAge = (double) (ages[0] + ages[1] + ages[2] + ages[3]) / 4;
+
+        int sum = 0;
+        for (int i = 0; i < ages.length; i++) {
+            sum += ages[i];
+        }
+        double averageAge = (double) sum / ages.length;
         System.out.println("Average age is: " + averageAge);
+
 
         ages = new int[4];
         ages[0] = 20;
