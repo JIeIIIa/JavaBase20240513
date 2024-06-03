c0c70717ac03257a4d8cc8520be6c40056ad189f
Onishehnko Oleksii <test@gmail.com>
2024-06-03 20:00:14 +0300

    Lesson-7 - Array (part-1)

--- com/gmail/onishchenko/lesson07/Lesson07Demo.java
+++ com/gmail/onishchenko/lesson07/Lesson07Demo.java
@@ -2,12 +2,13 @@ package com.gmail.onishchenko.lesson07;
 
 public class Lesson07Demo {
     public static void main(String[] args) {
-        int firstTourist = 20;
-        int secondTourist = 20;
-        int thirdTourist = 22;
-        int fourthTourist = 20;
+        int[] ages = new int[4];
+        ages[0] = 20;
+        ages[1] = 20;
+        ages[2] = 22;
+        ages[3] = 20;
 
-        double averageAge = (double) (firstTourist + secondTourist + thirdTourist + fourthTourist) / 4;
+        double averageAge = (double) (ages[0] + ages[1] + ages[2] + ages[3]) / 4;
         System.out.println("Average age is: " + averageAge);
     }
 }
