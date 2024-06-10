aaceae9c9a08cf80f3d5733ba2fa5fdcdf44a000
Onishehnko Oleksii <test@gmail.com>
2024-06-10 20:42:56 +0300

    Lesson-9 - Multi dimension array (part-2)

--- com/gmail/onishchenko/lessons/lesson09/Lesson09Demo.java
+++ com/gmail/onishchenko/lessons/lesson09/Lesson09Demo.java
@@ -4,13 +4,17 @@ public class Lesson09Demo {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4};
 
-        int[][] matrix = new int[2][3];
-        matrix[0] = new int[]{1, 2, 3};
-        matrix[1] = new int[]{4, 5, 6};
+        int[][] matrix = new int[3][];
+        matrix[0] = new int[]{1, 2, 3, 3, 3, 3, 3,3};
+        matrix[2] = new int[]{4, 5, 6, 7, 8, 9};
         System.out.println();
 
         for (int i = 0; i < matrix.length; i++) {
             int[] row = matrix[i];
+            if (row == null) {
+                System.out.println();
+                continue;
+            }
             for (int j = 0; j < row.length; j++) {
                 System.out.print(row[j] + " ");
             }
@@ -18,9 +22,10 @@ public class Lesson09Demo {
         }
 
         System.out.println("        Another approach");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        int[][] anotherMatrix = {{1, 2, 3}, {4, 5, 6}};
+        for (int i = 0; i < anotherMatrix.length; i++) {
+            for (int j = 0; j < anotherMatrix[i].length; j++) {
+                System.out.print(anotherMatrix[i][j] + " ");
             }
             System.out.println();
         }
