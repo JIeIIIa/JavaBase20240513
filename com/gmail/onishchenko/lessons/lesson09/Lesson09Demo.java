12e38214e6e94728ca2ea4f981f5179e43d5cc60
Onishehnko Oleksii <test@gmail.com>
2024-06-10 20:54:19 +0300

    Lesson-9 - Multi dimension array (part-3)

--- com/gmail/onishchenko/lessons/lesson09/Lesson09Demo.java
+++ com/gmail/onishchenko/lessons/lesson09/Lesson09Demo.java
@@ -1,5 +1,7 @@
 package com.gmail.onishchenko.lessons.lesson09;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Lesson09Demo {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4};
@@ -9,6 +11,33 @@ public class Lesson09Demo {
         matrix[2] = new int[]{4, 5, 6, 7, 8, 9};
         System.out.println();
 
+        print(matrix);
+
+        System.out.println("        Another approach");
+        int[][] anotherMatrix = {{1, 2, 3}, {4, 5, 6}};
+        for (int i = 0; i < anotherMatrix.length; i++) {
+            for (int j = 0; j < anotherMatrix[i].length; j++) {
+                System.out.print(anotherMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("        Random matrix");
+        int[][] randomMatrix = generateRandomMatrix(3, 5);
+        print(randomMatrix);
+    }
+
+    private static int[][] generateRandomMatrix(int rows, int columns) {
+        int[][] result = new int[rows][columns];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = ThreadLocalRandom.current().nextInt(100);
+            }
+        }
+        return result;
+    }
+
+    public static void print(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             int[] row = matrix[i];
             if (row == null) {
@@ -20,15 +49,5 @@ public class Lesson09Demo {
             }
             System.out.println();
         }
-
-        System.out.println("        Another approach");
-        int[][] anotherMatrix = {{1, 2, 3}, {4, 5, 6}};
-        for (int i = 0; i < anotherMatrix.length; i++) {
-            for (int j = 0; j < anotherMatrix[i].length; j++) {
-                System.out.print(anotherMatrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-
     }
 }
