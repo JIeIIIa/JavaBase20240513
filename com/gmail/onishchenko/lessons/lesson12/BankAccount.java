b57c6fcdf2e660df5dac43d5463ca7f0e6727c05
Onishehnko Oleksii <test@gmail.com>
2024-06-24 20:10:30 +0300

    Lesson-12 - Encapsulation and getters/setters (part-4)

--- com/gmail/onishchenko/lessons/lesson12/BankAccount.java
+++ com/gmail/onishchenko/lessons/lesson12/BankAccount.java
@@ -2,18 +2,24 @@ package com.gmail.onishchenko.lessons.lesson12;
 
 public class BankAccount {
     private int money;
+    private boolean active;
 
     public int getMoney() {
         return money;
     }
 
     void setMoney(int delta) {
-        if (money + delta < 0) {
-            return;
-        }
-
+        if (money + delta < 0) return;
         money += delta;
     }
 
-}
+    public boolean isActive() {
+        return active;
+    }
 
+    public void testMethod(int a,
+                           int b,
+                           int c) {
+
+    }
+}
