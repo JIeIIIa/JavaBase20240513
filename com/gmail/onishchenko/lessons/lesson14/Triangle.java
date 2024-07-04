4fffe7b13a57d8c8bf11bd3777f76d06ac5ca116
Onishehnko Oleksii <test@gmail.com>
2024-07-04 19:39:21 +0300

    Lesson-14 - Static nested classes

--- com/gmail/onishchenko/lessons/lesson14/Triangle.java
+++ com/gmail/onishchenko/lessons/lesson14/Triangle.java
@@ -3,10 +3,21 @@ package com.gmail.onishchenko.lessons.lesson14;
 import java.util.Arrays;
 
 public class Triangle {
-    private int[] sides;
+    private Edge[] sides;
 
     public Triangle(int a, int b, int c) {
-        this.sides = new int[]{a, b, c};
+        this.sides = new Edge[]{
+                new Edge(a, "black"),
+                new Edge(b, "black"),
+                new Edge(c, "black")
+        };
+    }
+
+    public Triangle(Edge[] sides) {
+        if (sides.length != 3) {
+            System.out.println("ERROR!!!");
+        }
+        this.sides = sides;
     }
 
     @Override
@@ -15,4 +26,23 @@ public class Triangle {
                 "sides=" + Arrays.toString(sides) +
                 '}';
     }
+
+    static class Edge {
+        private int length;
+        private String color;
+
+        public Edge(int length, String color) {
+            this.length = length;
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "length=" + length +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
 }
+
--- com/gmail/onishchenko/lessons/lesson14/TriangleRunner.java
+++ com/gmail/onishchenko/lessons/lesson14/TriangleRunner.java
@@ -4,5 +4,17 @@ public class TriangleRunner {
     public static void main(String[] args) {
         Triangle triangle = new Triangle(3, 4, 5);
         System.out.println(triangle);
+
+        System.out.println();
+        System.out.println(" -= Another triangle =-");
+        Triangle.Edge[] edges = {
+                new Triangle.Edge(1, "green"),
+                new Triangle.Edge(4, "black"),
+                new Triangle.Edge(2, "blue")
+        };
+        Triangle anotherTriangle = new Triangle(edges);
+        System.out.println(anotherTriangle);
+        edges[1] = new Triangle.Edge(-12345678, "white");
+        System.out.println(anotherTriangle);
     }
 }
